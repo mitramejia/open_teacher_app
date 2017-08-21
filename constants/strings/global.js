abef734e3ff33bb60f897afeb7074ea8e58e0858
Mitra Mejia <mitra.mejia@gmail.com>
2017-08-21 12:26:12 -0400

    Add error message for loadAssets function

--- constants/strings/global.js
+++ constants/strings/global.js
@@ -9,5 +9,7 @@
 // =============================================================
 
 export default {
-  appName: "Open Teacher"
+  appName: "Open Teacher",
+  loadAssetsErrorMessage: `There was an error caching assets (see: App.js), perhaps due to a 
+          network timeout, so we skipped caching. Reload the app to try again.`,
 }
