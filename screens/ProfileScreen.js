95421eafbdaa2c16703295f06cd22adbebb8748d
Mitra Mejia <mitra.mejia@gmail.com>
2017-08-20 21:12:53 -0400

    Add expo eslint

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {Button, Image, StyleSheet, View} from 'react-native';
-import {ImagePicker} from 'expo';
+import { Button, Image, StyleSheet, View } from 'react-native';
+import { ImagePicker } from 'expo';
 import strings from '../constants/strings/screens/ProfileScreen-strings';
-
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = {
@@ -13,16 +12,12 @@ export default class ProfileScreen extends React.Component {
   };
 
   render() {
-    let {image} = this.state;
+    let { image } = this.state;
 
     return (
       <View style={style.container}>
-        <Button
-          title={strings.heading}
-          onPress={this._pickImage}
-        />
-        {image &&
-        <Image source={{uri: image}} style={style.image}/>}
+        <Button title={strings.heading} onPress={this._pickImage} />
+        {image && <Image source={{ uri: image }} style={style.image} />}
       </View>
     );
   }
@@ -36,14 +31,12 @@ export default class ProfileScreen extends React.Component {
     console.log(result);
 
     if (!result.cancelled) {
-      this.setState({image: result.uri});
+      this.setState({ image: result.uri });
     }
   };
-
-
-};
+}
 
 const style = StyleSheet.create({
-  image: {width: 300, height: 300},
-  container: {flex: 1, alignItems: 'center', justifyContent: 'center'}
+  image: { width: 300, height: 300 },
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
 });
