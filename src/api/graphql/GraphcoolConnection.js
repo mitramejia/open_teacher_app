93cdc96f8c75a0786514c367a046224688d8baf7
Mitra Mejia <mitra.mejia@gmail.com>
2017-08-24 17:01:54 -0400

    Evaluate token

--- src/api/graphql/GraphcoolConnection.js
+++ src/api/graphql/GraphcoolConnection.js
@@ -11,11 +11,32 @@
 import { ApolloClient, createNetworkInterface } from 'apollo-client';
 import { GRAPHQL_SIMPLE_API_URL, WEB_SOCKET_CLIENT_URL } from 'react-native-dotenv';
 import { addGraphQLSubscriptions, SubscriptionClient } from 'subscriptions-transport-ws';
-
-// console.log(GRAPHQL_SIMPLE_API_URL);
-// console.log(WEB_SOCKET_CLIENT_URL);
+import { AsyncStorage } from 'react-native';
 
 const networkInterface = createNetworkInterface({ uri: GRAPHQL_SIMPLE_API_URL });
+
+// Use middleware to manage auth tokens
+networkInterface.use([
+  {
+    applyMiddleware(req, next) {
+      if (!req.options.headers) {
+        req.options.headers = {}; // Create the header object if needed.
+      }
+
+      AsyncStorage.getItem('token').then(
+        encodedToken => {
+          req.options.headers['authorization'] = `Bearer ${encodedToken}`;
+          next();
+        },
+        failure => {
+          console.error('ERROR: no token', failure);
+          next();
+        }
+      );
+    },
+  },
+]);
+
 // Create WebSocket client
 const wsClient = new SubscriptionClient(WEB_SOCKET_CLIENT_URL, {
   reconnect: true,
