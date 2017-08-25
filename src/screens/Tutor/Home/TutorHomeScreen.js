a4a13eef4149b023ad0debd8008f33d316162824
Mitra Mejia <mitra.mejia@gmail.com>
2017-08-25 18:24:31 -0400

    Add tutor home screen

---
+++ src/screens/Tutor/Home/TutorHomeScreen.js
@@ -0,0 +1,37 @@
+// =============================================================
+//
+// Name: StudentHomeScreen
+// -> Description:
+//
+// Author: mitramejia
+// Created at: 8/21/17
+//
+// =============================================================
+
+import React from 'react';
+import { Content, Button, Text } from 'native-base';
+import strings from './strings';
+import style from './style';
+
+export default class TutorHomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  static navigationOptions = {
+    title: 'TutorHomeScreen',
+  };
+
+  render() {
+    return (
+      <Content contentContainerStyle={style.container}>
+        <Button>
+          <Text>
+            {strings.test}
+          </Text>
+        </Button>
+      </Content>
+    );
+  }
+}
