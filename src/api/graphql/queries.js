b0fa6f038d9347695b23ffccb5aaf43ff9e3e0ee
Mitra Mejia <mitra.mejia@gmail.com>
2017-08-25 18:24:52 -0400

    Move all graphql queries to it's own file

---
+++ src/api/graphql/queries.js
@@ -0,0 +1,43 @@
+// =============================================================
+//
+// Name: model
+// -> Description:
+//
+// Author: mitramejia
+// Created at: 8/25/17
+//
+// =============================================================
+
+import { gql } from 'react-apollo';
+
+export const createAccount = gql`
+  mutation($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    createUser(
+      authProvider: { email: { email: $email, password: $password } }
+      firstName: $firstName
+      lastName: $lastName
+    ) {
+      id
+      firstName
+      lastName
+      email
+      password
+    }
+  }
+`;
+
+export const userQuery = gql`
+  query {
+    user {
+      id
+    }
+  }
+`;
+
+export const signinUser = gql`
+  mutation($email: String!, $password: String!) {
+    signinUser(email: { email: $email, password: $password }) {
+      token
+    }
+  }
+`;
