b2858bc9e3a14172f944ed75e9bf35513b04dc58
Mitra Mejia <mitra.mejia@gmail.com>
2017-08-25 18:26:33 -0400

    Route users depending on their state

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,36 +1,63 @@
 import React from 'react';
+import { AppLoading } from 'expo';
 import { Button, Text, Content } from 'native-base';
 import { ApolloProvider, compose, gql, graphql, withApollo } from 'react-apollo';
+import PropTypes from 'prop-types';
 import StudentHomeScreen from '../Student/Home/StudentHomeScreen';
 import LoginScreen from '../Login/LoginScreen';
+import TutorHomeScreen from '../Tutor/Home/TutorHomeScreen';
+import { userQuery } from '../../api/graphql/queries';
 
 class HomeScreen extends React.Component {
+  state = {
+    user: null,
+  };
+
   static navigationOptions = {
     header: null,
   };
 
+  _userIsLoggedIn = () => {
+    return this.props.data.user;
+  };
+
+  _userIsTutor = () => {
+    return this.props.data.user.isTutor;
+  };
+
   render() {
-    return (
-      <StudentHomeScreen
-        client={this.props.client}
-        // user={this.props.data.user}
-        navigation={this.props.navigation}
-      />
-    );
+    if (this.props.data.loading) {
+      return <AppLoading />;
+    } else {
+      if (this._userIsLoggedIn()) {
+        if (this._userIsTutor()) {
+          return (
+            <TutorHomeScreen
+              client={this.props.client}
+              user={this.props.data.user}
+              navigation={this.props.navigation}
+            />
+          );
+        } else {
+          return (
+            <StudentHomeScreen
+              client={this.props.client}
+              user={this.props.data.user}
+              navigation={this.props.navigation}
+            />
+          );
+        }
+      } else {
+        return <LoginScreen client={this.props.client} navigation={this.props.navigation} />;
+      }
+    }
   }
 }
 
-const userQuery = gql`
-  query {
-    user {
-      id
-      firstName
-      lastName
-      email
-      birthdate
-    }
-  }
-`;
+HomeScreen.propTypes = {
+  client: React.PropTypes.object.isRequired,
+  navigation: React.PropTypes.object.isRequired,
+};
 
 export default graphql(userQuery, { options: { fetchPolicy: 'network-only' } })(
   withApollo(HomeScreen)
