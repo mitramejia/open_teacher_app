2e7f2ffcc53430b78dc5629f0b426406fa2139df
Mitra Mejia <mitra.mejia@gmail.com>
2017-08-28 02:27:53 -0400

    Update tests

---
+++ src/__tests__/GraphQLQueries.spec.js
@@ -0,0 +1,23 @@
+// =============================================================
+//
+// Name: Queries-test
+// -> Description:
+//
+// Author: mitramejia
+// Created at: 8/28/17
+//
+// =============================================================
+
+import { userQuery, createAccount, signinUser } from '../api/graphql/queries';
+
+it('userQuery should be the correct query', () => {
+  expect(userQuery).toMatchSnapshot();
+});
+
+it('createAccount should be the correct query', () => {
+  expect(createAccount).toMatchSnapshot();
+});
+
+it('signinUser should be the correct query', () => {
+  expect(signinUser).toMatchSnapshot();
+});
---
+++ src/__tests__/TutorHomeScreen.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TutorHomeScreen from '../screens/Tutor/Home/TutorHomeScreen';
+import renderer from 'react-test-renderer';
+import { ApolloProvider } from 'react-apollo';
+import { mockApolloClient } from '../api/graphql/graphql-test-schema';
+
+it('renders the Home Screen', () => {
+  const navigation = {};
+  const user = {};
+  const tree = renderer
+    .create(
+      <ApolloProvider client={mockApolloClient()}>
+        <TutorHomeScreen navigation={navigation} user={user} />
+      </ApolloProvider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
