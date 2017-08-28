3e82bc36f9b1bf4ac10be8aece964ed84592f86e
Mitra Mejia <mitra.mejia@gmail.com>
2017-08-28 19:29:08 -0400

    Add secondary and white color

--- src/config/style.js
+++ src/config/style.js
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export const colors = {
   primary: '#1D374C',
+  secondary: '#5090CD',
   tintColor: '#2f95dc',
   tabIconDefault: '#ccc',
   tabIconSelected: '#2f95dc',
@@ -12,6 +13,7 @@ export const colors = {
   warningText: '#666804',
   noticeBackground: '#2f95dc',
   noticeText: '#fff',
+  white: '#FAFCFF',
 };
 
 export const styles = StyleSheet.create({
