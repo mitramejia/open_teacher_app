51ee55bd207ab965a167b3142da7e46c60363b53
Mitra Mejia <mitra.mejia@gmail.com>
2017-09-01 16:13:44 -0400

    Move data handling to parent component

--- src/screens/Student/Home/Form/TutorSearchForm.js
+++ src/screens/Student/Home/Form/TutorSearchForm.js
@@ -28,53 +28,38 @@ import strings from '../strings';
 import style from '../style';
 import { colors } from '../../../../config/style';
 import OptionPicker from '../../../../components/OptionPicker';
-import { allSubjectsQuery } from '../../../../api/graphql/queries';
-import { graphql, withApollo } from 'react-apollo';
-import { AppLoading } from 'expo';
 
 class TutorSearchForm extends React.Component {
   render() {
-    if (this.props.allSubjectsQuery.loading) {
-      return <AppLoading />;
-    } else {
-      return (
-        <Content padder style={style.formContainer}>
-          <View style={style.formContainer}>
-            <Icon
-              style={style.userIcon}
-              name="ios-contact-outline"
-              size={44}
-              color={colors.secondary}
+    return (
+      <Content padder style={style.formContainer}>
+        <View style={style.formContainer}>
+          <Icon
+            style={style.userIcon}
+            name="ios-contact-outline"
+            size={44}
+            color={colors.secondary}
+          />
+        </View>
+        <Form ref="registrationForm">
+          <OptionPicker name="Materias" icon="ios-book-outline" data={this.props.subjects} />
+          <Item style={style.inputField} regular>
+            <Icon name="ios-calendar-outline" size={28} color={colors.secondary} />
+            <Input
+              placeholder={strings.timeInputField}
+              ref={input => {
+                this.time = input;
+              }}
             />
-          </View>
-          <Form ref="registrationForm">
-            <OptionPicker
-              name="Materias"
-              icon="ios-book-outline"
-              data={this.props.allSubjectsQuery.allSubjects}
-            />
-            <Item style={style.inputField} regular>
-              <Icon name="ios-calendar-outline" size={28} color={colors.secondary} />
-              <Input
-                placeholder={strings.timeInputField}
-                ref={input => {
-                  this.time = input;
-                }}
-              />
-            </Item>
-          </Form>
-        </Content>
-      );
-    }
+          </Item>
+        </Form>
+      </Content>
+    );
   }
 }
 
 TutorSearchForm.propTypes = {
-  client: React.PropTypes.object.isRequired,
-  allSubjectsQuery: PropTypes.shape({
-    loading: PropTypes.bool.isRequired,
-    allSubjects: PropTypes.array,
-  }).isRequired,
+  subjects: React.PropTypes.array.isRequired,
 };
 
-export default graphql(allSubjectsQuery, { name: 'allSubjectsQuery' })(withApollo(TutorSearchForm));
+export default TutorSearchForm;
--- src/screens/Student/Home/StudentHomeScreen.js
+++ src/screens/Student/Home/StudentHomeScreen.js
@@ -10,9 +10,12 @@
 
 import React from 'react';
 import { Container, Content, Header, Form, Item, Input, Toast, Button } from 'native-base';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import style from './style';
 import TutorSearchForm from './Form/TutorSearchForm';
+import { allSubjectsQuery } from '../../../api/graphql/queries';
+import { graphql, withApollo } from 'react-apollo';
+import { AppLoading } from 'expo';
 
 class StudentHomeScreen extends React.Component {
   static navigationOptions = {
@@ -20,18 +23,27 @@ class StudentHomeScreen extends React.Component {
   };
 
   render() {
-    return (
-      <Content contentContainerStyle={style.container}>
-        <TutorSearchForm client={this.props.client} />
-      </Content>
-    );
+    if (this.props.allSubjectsQuery.loading) {
+      return <AppLoading />;
+    } else {
+      return (
+        <Content contentContainerStyle={style.container}>
+          <TutorSearchForm subjects={this.props.allSubjectsQuery.allSubjects} />
+        </Content>
+      );
+    }
   }
 }
 
 StudentHomeScreen.propTypes = {
   client: React.PropTypes.object.isRequired,
-  navigation: React.PropTypes.object.isRequired,
   user: React.PropTypes.object.isRequired,
+  allSubjectsQuery: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    allSubjects: PropTypes.array,
+  }).isRequired,
 };
 
-export default StudentHomeScreen;
+export default graphql(allSubjectsQuery, { name: 'allSubjectsQuery' })(
+  withApollo(StudentHomeScreen)
+);
